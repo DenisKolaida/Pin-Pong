1e591438c35662b810e65b64917728762783e2a4
DenisKolaida <nat3330777@gmail.com>
2023-11-18 14:46:23 +0300

    update

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ win = pg.display.set_mode((W, H))
 pg.display.set_caption('Пинг-понг')
 clock = pg.time.Clock()
 
+ball = pg.image.load('images/ball.png')
+ballr = ball.get_rect(topleft = (350, 350))
+
 while True:
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+
+    win.fill((40, 230, 241))
+    win.blit(ball, ballr)
 
     pg.display.update()
     clock.tick(FPS)
